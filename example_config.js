b716c063aec268d53e70de3d10a9f9b5523128e1
TigerMehMat <vovamehmat@gmail.com>
2018-12-26 19:26:15 +0300

    Create example_config.js

---
+++ example_config.js
@@ -0,0 +1,8 @@
+{
+	"protocol": "https",// Протокол соединения
+	"server": "ark-ru.gamepedia.com",// Сайт для соединения
+	"path": "",// Для MediaWiki оставить пустым
+	"debug": false,// Не изменять
+	"username": "",// Логин
+	"password": ""// Пароль
+}
